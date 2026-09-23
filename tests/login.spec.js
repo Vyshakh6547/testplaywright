7a91784928518f2ec4f757a99b21f9c8707e9f7d
Vyshakh6547 <vyshakh6547@gmail.com>
2026-09-23 15:05:31 -0700

    refactor: use environment variables for username and password in login test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,9 +1,11 @@
 const {test,expect} = require('@playwright/test')
+const username = process.env.USERNAME
+const password = process.env.PASSWORD
 
 test('test', async ({ page }) => {
     await page.goto('https://thedouble.ai/auth/signin');
-    await page.getByPlaceholder("name@domain.com").fill("username")
-    await page.locator("//input[@id='password']").fill("password")
+    await page.getByPlaceholder("name@domain.com").fill(username)
+    await page.locator("//input[@id='password']").fill(password)
     await page.locator("//button[@type='submit']").click()
     await expect(page).toHaveURL(/home/);
     await page.getByLabel("User profile and usage indicator").nth(1).click()
